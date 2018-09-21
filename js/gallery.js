96f41ec83522dbd1b20d1d10a59caf54ab381cc4
Peter Sichel <psichel@rbmtechnologies.com>
2018-09-21 14:59:41 -0400

    pre-download large images

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,25 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
   //alert("Hello from the gallery file!");
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+  
   let galleryInfo = document.querySelector("#gallery-info");
   let title       = galleryInfo.querySelector(".title");
   let description = galleryInfo.querySelector(".description");
 
   thumbnails.forEach(function(thumbnail) {
+        // Preload large images.
+        let newImageSrc  = thumbnail.dataset.largeVersion;
+        let largeVersion = new Image();
+        largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
       // code to set clicked image as main image      
       let newImageSrc = thumbnail.dataset.largeVersion;
       let newImageAlt = thumbnail.dataset.title;
-      let mainImage = document.querySelector("#gallery-photo > img");
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", newImageAlt);
       // update aside title and description
